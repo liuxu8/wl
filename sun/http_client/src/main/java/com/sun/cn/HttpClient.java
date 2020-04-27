71f35a5afa676316cfcd843044ee762160640387
liuxu8 <892243561@qq.com>
2020-04-27 22:18:54 +0800

    wl

--- sun/http_client/src/main/java/com/sun/cn/HttpClient.java
+++ sun/http_client/src/main/java/com/sun/cn/HttpClient.java
@@ -1,5 +1,28 @@
 package com.sun.cn;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.Test;
+
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
 public class HttpClient {
+    @Test
+    public void test1() throws IOException {
+    //用来存放我们的结果
+        String result =null;
+        HttpGet get = new HttpGet("http://www.baidu.com");
+        //之后执行get方法
+        DefaultHttpClient client = new DefaultHttpClient();
+        HttpResponse response = client.execute(get);
+        result = EntityUtils.toString(response.getEntity(),Charset.defaultCharset());
+        System.out.println(result);
+
+
+    }
 
 }
